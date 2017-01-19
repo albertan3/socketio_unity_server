c4d586d5e3666a7de6519cd4d2f31939992fed02
An, Albert <alban@visa.com>
2017-01-18 17:57:26 -0800

    test2

--- server.js
+++ server.js
@@ -1,48 +1,21 @@
-var express = require('express');
-var app = express();
-var server = require('http').createServer(app);
-var io = require('socket.io')().listen(server);
+'use strict';
 
-io.on("connection", function(socket){
-    console.log("A new client has connected with the id " + socket.id + "!");
-    
-    socket.on("disconnect", function(){
-        console.log("the client has disconnected!");
-    });
-    
-    socket.on("Message", function(data){
-        console.log(data.message);
-        
-        io.emit("Message", data)
-        
-    });
-    
-    socket.on("Private Message", function(data){
-        console.log(data);
-        
-        io.emit(data.chatID, data)
-        
-    });
-    
-  
-    
-    socket.on('typing', function(data){
-        io.emit(data.chatID+'-typing', data);
-    });
-    
-    socket.on('stop typing', function(data){
-        io.emit(data.chatID+'-stop typing', data);
-    });
-})
+const express = require('express');
+const socketIO = require('socket.io');
+const path = require('path');
 
-var PORT = process.env.PORT || 3000;
-server.listen(PORT, function(){
-    console.log("Listening on PORT " + process.env.PORT);
-})
- 
- /*
-var PORT = process.env.OPENSHIFT_NODEJS_PORT || 3000;
-var IP = process.env.OPENSHIFT_NODEJS_IP;
-server.listen(PORT,IP, function(){
-    console.log("Listening on PORT " + PORT);
-});*/
+const PORT = process.env.PORT || 3000;
+const INDEX = path.join(__dirname, 'index.html');
+
+const server = express()
+  .use((req, res) => res.sendFile(INDEX) )
+  .listen(PORT, () => console.log(`Listening on ${ PORT }`));
+
+const io = socketIO(server);
+
+io.on('connection', (socket) => {
+  console.log('Client connected');
+  socket.on('disconnect', () => console.log('Client disconnected'));
+});
+
+setInterval(() => io.emit('time', new Date().toTimeString()), 1000);
