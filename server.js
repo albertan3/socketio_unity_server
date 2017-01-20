ab205b8c3c12542fb08a895fc00c710678a8b384
An, Albert <alban@visa.com>
2017-01-19 19:38:26 -0800

    for unity server change

--- server.js
+++ server.js
@@ -1,21 +1,74 @@
-'use strict';
+var io 				= require('socket.io')(4567);
+var shortId 		= require('shortid');
 
-const express = require('express');
-const socketIO = require('socket.io');
-const path = require('path');
 
-const PORT = process.env.PORT || 3000;
-const INDEX = path.join(__dirname, 'index.html');
+var clients			= [];
 
-const server = express()
-  .use((req, res) => res.sendFile(INDEX) )
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`));
+io.on('connection', function (socket) {
+	
+	var currentUser;
 
-const io = socketIO(server);
+	socket.on('USER_CONNECT', function (){
 
-io.on('connection', (socket) => {
-  console.log('Client connected');
-  socket.on('disconnect', () => console.log('Client disconnected'));
+		console.log('Users Connected ');
+		for (var i = 0; i < clients.length; i++) {
+			
+			
+			socket.emit('USER_CONNECTED',{
+
+				name:clients[i].name,
+				id:clients[i].id,
+				position:clients[i].position
+
+			});
+
+			console.log('User name '+clients[i].name+' is connected..');
+
+		};
+
+	});
+
+	socket.on('PLAY', function (data){
+		currentUser = {
+			name:data.name,
+			id:shortId.generate(),
+			position:data.position
+		}
+
+		clients.push(currentUser);
+		socket.emit('PLAY',currentUser );
+		socket.broadcast.emit('USER_CONNECTED',currentUser);
+
+	});
+
+	socket.on('disconnect', function (){
+
+		socket.broadcast.emit('USER_DISCONNECTED',currentUser);
+		for (var i = 0; i < clients.length; i++) {
+			if (clients[i].name === currentUser.name && clients[i].id === currentUser.id) {
+
+				console.log("User "+clients[i].name+" id: "+clients[i].id+" has disconnected");
+				clients.splice(i,1);
+
+			};
+		};
+
+	});
+
+	socket.on('MOVE', function (data){
+
+		// currentUser.name = data.name;
+		// currentUser.id   = data.id;
+		currentUser.position = data.position;
+
+		socket.broadcast.emit('MOVE', currentUser);
+		console.log(currentUser.name+" Move to "+currentUser.position);
+
+
+	});
+
+
 });
 
-setInterval(() => io.emit('time', new Date().toTimeString()), 1000);
+
+console.log("------- server is running -------");
