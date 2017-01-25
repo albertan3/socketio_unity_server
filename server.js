40b0abe3ee158a7157a0a9db041493b5715b25d9
An, Albert <alban@visa.com>
2017-01-25 10:58:11 -0800

    socket io that works

--- server.js
+++ server.js
@@ -1,83 +1,21 @@
-var express = require('express');
-var io = require('socket.io'); //(4567);
-var shortId = require('shortid');
-var path = require('path');
-var PORT = process.env.PORT || 4567;
-var INDEX = path.join(__dirname, 'index.html');
+'use strict';
 
-var clients			= [];
+const express = require('express');
+const socketIO = require('socket.io');
+const path = require('path');
 
-var server = express()
-				.use((req, res)=> res.sendFile(INDEX))
-				.listen(PORT, () =>console.log('Listening on ${PORT}'));
+const PORT = process.env.PORT || 3000;
+const INDEX = path.join(__dirname, 'index.html');
 
-io.on('connection', function (socket) {
-	
-	var currentUser;
+const server = express()
+  .use((req, res) => res.sendFile(INDEX) )
+  .listen(PORT, () => console.log(`Listening on ${ PORT }`));
 
-	socket.on('USER_CONNECT', function (){
+const io = socketIO(server);
 
-		console.log('Users Connected ');
-		for (var i = 0; i < clients.length; i++) {
-			
-			
-			socket.emit('USER_CONNECTED',{
-
-				name:clients[i].name,
-				id:clients[i].id,
-				position:clients[i].position
-
-			});
-
-			console.log('User name '+clients[i].name+' is connected..');
-
-		};
-
-	});
-
-	socket.on('PLAY', function (data){
-		currentUser = {
-			name:data.name,
-			id:shortId.generate(),
-			position:data.position
-		}
-
-		clients.push(currentUser);
-		socket.emit('PLAY',currentUser );
-		socket.broadcast.emit('USER_CONNECTED',currentUser);
-
-	});
-
-	socket.on('disconnect', function (){
-
-		socket.broadcast.emit('USER_DISCONNECTED',currentUser);
-		for (var i = 0; i < clients.length; i++) {
-			if (clients[i].name === currentUser.name && clients[i].id === currentUser.id) {
-
-				console.log("User "+clients[i].name+" id: "+clients[i].id+" has disconnected");
-				clients.splice(i,1);
-
-			};
-		};
-
-	});
-
-	socket.on('MOVE', function (data){
-
-		// currentUser.name = data.name;
-		// currentUser.id   = data.id;
-		currentUser.position = data.position;
-
-		socket.broadcast.emit('MOVE', currentUser);
-		console.log(currentUser.name+" Move to "+currentUser.position);
-
-
-	});
-
-
+io.on('connection', (socket) => {
+  console.log('Client connected');
+  socket.on('disconnect', () => console.log('Client disconnected'));
 });
 
 setInterval(() => io.emit('time', new Date().toTimeString()), 1000);
-
-
-console.log("------- server is running -------");
